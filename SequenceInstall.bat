a0c456ca1f59bc706f048560bd4b3d3e54d33749
SeqGamer <nutsboltssg@gmail.com>
2024-07-04 09:16:35 -0600

    Add batch file

---
+++ SequenceInstall.bat
@@ -0,0 +1,12 @@
+cd C:\
+git clone https://Github.com/SeqGamer/SequenceGameStates
+rem @echo off
+rem echo Set oWS = WScript.CreateObject("WScript.Shell") > C:\temp\CreateShortcut.vbs
+rem echo sLinkFile = "C:\temp\Sequence.lnk" >> C:\temp\CreateShortcut.vbs
+rem echo Set oLink = oWS.CreateShortcut(sLinkFile) >> C:\temp\CreateShortcut.vbs
+rem echo oLink.TargetPath = "C:\SequenceGameStates\SequencePlay.exe" >> C:\temp\CreateShortcut.vbs
+rem echo oLink.Save >> C:\temp\CreateShortcut.vbs
+rem cscript C:\temp\CreateShortcut.vbs
+rem del C:\temp\CreateShortcut.vbs
+rem copy "C:\temp\Sequence.lnk" "%AppData%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Sequence.lnk"
+rem pause
